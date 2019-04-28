891da968d609a6669a61200d7eb970783e4203a5
Zhilian DAI <daizhilian@hotmail.com>
2019-04-28 14:53:56 +0800

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.0/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.1c7a63ccaa683928ea14f95e037727b8.js"
+  "./precache-manifest.7a413c649f20ec9f468ffb3d0f1f17bd.js"
 );
 
 self.addEventListener('message', (event) => {

adb45ccac1c6615bbba7384074f0835e0819edce
Zhilian DAI <daizhilian@hotmail.com>
2019-04-28 13:06:22 +0800

    Updates

---
+++ precache-manifest.7f030775dae4e042c0fe083980dd67ad.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dc0c237f080684da6aaef040f4e28182",
+    "url": "./home/index.html"
+  },
+  {
+    "revision": "8bb83ca42755ccddec6d",
+    "url": "./home/static/css/main.98f075c1.chunk.css"
+  },
+  {
+    "revision": "5bcd468ad4f606d97936",
+    "url": "./home/static/js/2.97077daf.chunk.js"
+  },
+  {
+    "revision": "8bb83ca42755ccddec6d",
+    "url": "./home/static/js/main.a816b11f.chunk.js"
+  },
+  {
+    "revision": "422072e81b8ed6307378",
+    "url": "./home/static/js/runtime~main.23e65c10.js"
+  }
+]);
